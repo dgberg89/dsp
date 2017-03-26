693e89d61c613a757c9320f510fa63cf3dab6f53
Douglas Goldberg <gberg@Douglass-MacBook-Pro.local>
2017-03-26 01:19:21 -0700

    Uploaded text files to Editors

---
+++ editors/egg.py
@@ -0,0 +1,4 @@
+print("Learning to create a file with emacs is challenging.")
+print("But fantastic when the job is complete.")
+
+
---
+++ editors/vinegar.py
@@ -0,0 +1,4 @@
+
+print("Very glad both emacs and vim were already on my laptop.")
+print("It has made this task a lot simpler.")
+
