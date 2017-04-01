3b215a42ce23b6debf68e43cde6cb40780916da9
dgberg89 <dgberg@pacbell.net>
2017-03-31 22:58:24 -0700

    Update advanced_python_csv.py
    Added Python code to send email list to new csv file called emails.csv.

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,5 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+    
+Faculty = pd.read_csv('faculty.csv')
+Faculty = Faculty.rename(columns = {'name' : 'Name', ' degree' : 'Degree', ' title' : 'Title', ' email' : 'Email'})
+Faculty['Email'].to_csv('emails.csv', index=False)
